05e80ca251e5f53fe0853bd491825f6a3b4ca2b1
SURPAT_Enjoyer <mitya26041986@gmail.com>
2024-07-16 22:54:20 +0300

    homework 2.2.1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,41 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.CarService;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
+
+      carService.add(new Car(new Random().nextInt(), "model_1"));
+      carService.add(new Car(new Random().nextInt(), "model_2"));
+      List<Car> cars = carService.listCars();
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", cars.get(0)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", cars.get(1)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car series = "+user.getCar().getSeries());
+         System.out.println("Car model = "+user.getCar().getModel());
+         System.out.println();
+      }
+      System.out.println(userService.getUserByCarModelAndSeries("model_1", 12345));
+      context.close();
+   }
+}
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDao.java
@@ -0,0 +1,10 @@
+package hiber.dao;
+
+import hiber.model.Car;
+
+import java.util.List;
+
+public interface CarDao {
+    void add(Car Car);
+    List<Car> listCars();
+}
